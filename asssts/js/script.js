a25299f6a5de9cc93dc1b04fd3b27bc92e2a039a
unknown <NayeemWeb5848>
2023-08-06 00:59:52 +0600

    first commit

---
+++ asssts/js/script.js
@@ -0,0 +1,12 @@
+/*============================================= Auto Type Start ========================================= */
+var typed = new Typed(".typing",{
+     strings:["Web Desinger","Web Developer", "Video Editor","Contant Creator"],
+     typeSpeed:150,
+     BackSpeed:60,
+     loop:true
+})
+
+$(function(){
+     'use strict';
+     $('.venobox').venobox()
+});
